cc6a14de462dd6fe2521b49aed206e5c335c83a1
viisualworks <dshege4@gmail.com>
2024-12-12 14:25:24 +0300

    add custom colors and fonts to the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./*.{php,html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grey: "#3B3F42",
+        "light-grey": "#F2F2F2",
+        orange: "#E39C04",
+        white: "#ffffff"
+      }
+    },
   },
   plugins: [],
 }
