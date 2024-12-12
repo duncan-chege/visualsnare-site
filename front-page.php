3aaa359dbdb618b85cb957b0e63431041092ec88
viisualworks <dshege4@gmail.com>
2024-12-12 11:47:16 +0300

    add tailwind css to my project

--- front-page.php
+++ front-page.php
@@ -1,17 +1,18 @@
-<?php 
+<?php
 
-    get_header();
-?>
+get_header(); ?>
 
 <div class="front-page-hero"> </div>
 
 <?php
-    while(have_posts()){
-        the_post(); ?>
-<h2> <?php the_title(); ?></h2>
+while (have_posts()) {
+    the_post(); ?>
+<h2 class="bg-blue-400"> <?php the_title(); ?></h2>
 <?php the_content(); ?>
 
-<?php }
+<?php
+}
 
-    get_footer();
+get_footer();
+
 ?>
--- functions.php
+++ functions.php
@@ -1,13 +1,8 @@
 <?php 
     function main_files(){
-        wp_enqueue_style('custom-google-font', '//fonts.googleapis.com/css2?family=Quicksand:wght@400;600;700&display=swap', false);
-        wp_enqueue_style('bootstrap5', "//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css");
-        wp_enqueue_style('custom-style',  get_template_directory_uri().'/assets/css/custom.css');
+        wp_enqueue_style( 'output', get_template_directory_uri() . '/dist/output.css', array() );
         wp_enqueue_style('main-css', get_stylesheet_uri());
     
-        wp_enqueue_script( 'jQuery','//code.jquery.com/jquery-3.2.1.slim.min.js', array('jquery'));
-        wp_enqueue_script( 'popper','//cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js');
-        wp_enqueue_script( 'bootstrap-js','//cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js' );
         wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/js/scripts.js', array('jquery'), null, true );
     }
 
