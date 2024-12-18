fcb67e1b2da72c358b7c1cb9ac20d1f767bd193e
viisualworks <dshege4@gmail.com>
2024-12-18 17:17:20 +0300

    edit the single.php file and create a new one

--- single.php
+++ single.php
@@ -1,16 +1,14 @@
 <?php get_header(); ?>
 
-<?php while(have_posts()){ the_post(); ?>
+<?php while (have_posts()) {
+    the_post(); ?>
 
-<div class='container'>
-    <div class='row'>
-        <div class='col-md-12'>
-            <h1 class='font-weight-bold mt-3'><?php echo get_the_title(); ?></h1>
-            <?php the_content(); ?>
-        </div>
-    </div>
+<div>
+    <h1 class='font-weight-bold mt-3'><?php echo get_the_title(); ?></h1>
+    <?php the_content(); ?>
 </div>
 
-<?php } ?>
+<?php
+} ?>
 
 <?php get_footer(); ?>
